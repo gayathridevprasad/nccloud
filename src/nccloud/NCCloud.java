2409cb2a0b68da35318bee0f8249624d62344c7f
Kayathri Devi Devprasad <gayathridevprasad2007@gmail.com>
2022-08-10 22:41:39 +0530

    first commit

---
+++ src/nccloud/NCCloud.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nccloud;
+
+/**
+ *
+ * @author mithya
+ */
+public class NCCloud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
